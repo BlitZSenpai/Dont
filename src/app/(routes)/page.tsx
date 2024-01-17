05ab1f542fd6625a0ed16c50092d996099d9e1d2
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-17 23:26:04 +0530

    add: test cases

---
+++ src/app/(routes)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+export default function Home() {
+  return (
+    <div>
+      <Input placeholder="Enter medium blog url" onChange={() => {}} />
+      <Button onClick={() => {}}>Search</Button>
+    </div>
+  );
+}
