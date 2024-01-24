c79b4e87477caed2f4f0ba7b111ac8edbc1f4a35
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-24 22:54:35 +0530

    styles

--- src/app/_components/default-navbar.tsx
+++ src/app/_components/default-navbar.tsx
@@ -4,7 +4,7 @@ import { OtherStuff } from "./otherstuff";
 
 export const DefaultNavbar = () => {
   return (
-    <div className="inset-x-0 top-0 dark:bg-neutral-950/70 backdrop:blur-md opacity-70 fixed">
+    <div className="inset-x-0 top-0 dark:bg-neutral-950/70 backdrop:blur-md fixed">
       <div className="w-full flex justify-between p-6 px-16 items-center">
         <Logo />
         <Links />
--- src/app/_components/links.tsx
+++ src/app/_components/links.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 export const Links = () => {
   return (
     <div className="space-x-10">
-      <Link href="/about" className="font-semibold hover:text-white text-xl dark:text-muted-foreground">
+      <Link href="/about" className="font-normal hover:opacity-100 text-lg dark:text-muted-foreground">
         About
       </Link>
-      <Link href="/check" className="font-semibold text-xl dark:text-muted-foreground">
+      <Link href="/check" className="font-noamrl text-lg dark:text-muted-foreground">
         Check
       </Link>
-      <Link href="/something" className="font-semibold text-xl dark:text-muted-foreground">
+      <Link href="/something" className="font-normal text-lg dark:text-muted-foreground">
         Something
       </Link>
     </div>
