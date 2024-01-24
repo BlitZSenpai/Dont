5d657fa6721411ab74cc99af5eba93e7185bd6a1
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-24 22:38:35 +0530

    add: navbar links

--- src/app/_components/default-navbar.tsx
+++ src/app/_components/default-navbar.tsx
@@ -1,5 +1,4 @@
 import { Logo } from "@/components/logo";
-import { ModeToggle } from "./darkmode-toggle";
 import { Links } from "./links";
 import { OtherStuff } from "./otherstuff";
 
--- src/app/_components/links.tsx
+++ src/app/_components/links.tsx
@@ -2,9 +2,15 @@ import Link from "next/link";
 
 export const Links = () => {
   return (
-    <div>
-      <Link href="/about" className="font-semibold text-xl dark:text-neutral-200">
+    <div className="space-x-10">
+      <Link href="/about" className="font-semibold hover:text-white text-xl dark:text-muted-foreground">
         About
+      </Link>
+      <Link href="/check" className="font-semibold text-xl dark:text-muted-foreground">
+        Check
+      </Link>
+      <Link href="/something" className="font-semibold text-xl dark:text-muted-foreground">
+        Something
       </Link>
     </div>
   );
