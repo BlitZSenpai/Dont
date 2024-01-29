c1f94aff9c244b6f7b1efba89c8e5f2f4a526689
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-29 21:17:46 +0530

    add: bg gradient

--- src/app/(routes)/about/page.tsx
+++ src/app/(routes)/about/page.tsx
@@ -35,6 +35,7 @@ const AboutPage = () => {
         think to do was to make this video in hope that the world will finally see this man, for what he
         really is.
       </p>
+      {/* drop shadow in the link class */}
       <Link
         className="text-lg font-medium bg-gradient-to-r from-fuchsia-500 via-red-600 to-orange-400 bg-clip-text text-transparent hover:animate-pulse duration-500 transition-all"
         href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
--- src/app/_components/hero-text.tsx
+++ src/app/_components/hero-text.tsx
@@ -1,7 +1,7 @@
 export const HeroText = () => {
   return (
     <div className="pt-20 flex flex-col items-start justify-center mb-3">
-      <h1 className="text-6xl font-bold">Just Dont.</h1>
+      <h1 className="text-6xl font-bold dark:text-white">Just Dont.</h1>
       <p className="dark:text-neutral-200 text-lg">Enter a members only medium blog link.</p>
     </div>
   );
