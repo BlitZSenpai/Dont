0970133b250d060fcef72d77928c99f503ebbf77
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-01-30 23:09:17 +0530

    add: mock text

--- src/app/(routes)/check/page.tsx
+++ src/app/(routes)/check/page.tsx
@@ -2,6 +2,7 @@ const CheckPage = () => {
   return (
     <div className="h-dvh items-center justify-center">
       <h1 className="text-6xl font-bold">Work in Progress!</h1>
+      <p className="text-lg font-medium">Page only acts as a mock checking page</p>
     </div>
   );
 };
--- src/app/(routes)/something/page.tsx
+++ src/app/(routes)/something/page.tsx
@@ -2,6 +2,7 @@ const SomethingPage = () => {
   return (
     <div className="h-dvh flex items-center justify-center">
       <h1 className="text-6xl font-bold">Work in Progress!</h1>
+      <p className="text-lg font-medium">SomethingPage content goes here</p>
     </div>
   );
 };
